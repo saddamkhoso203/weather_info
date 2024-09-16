5524dbc0545d2f352b82b2e724ec3c6ae6f6a2c7
BCCK <saddamkhoso203@gmail.com>
2024-09-16 06:29:50 +0500

    ui almost completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'package:intl/intl.dart';
+import 'package:weather_info/consts/colors.dart';
+import 'package:weather_info/consts/images.dart';
+import 'package:weather_info/consts/strings.dart';
+import 'package:weather_info/models/current_weather_model.dart';
+
+import 'consts/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +16,214 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      theme: ThemeData(fontFamily: 'poppins'),
+      debugShowCheckedModeBanner: false,
+      home: const WeatherInfo(),
       title: 'Weather Info',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class WeatherInfo extends StatelessWidget {
+  const WeatherInfo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var date = DateFormat("yMMMd").format(DateTime.now());
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: "$date".text.gray700.make(),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.light_mode,
+                color: Vx.gray600,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
+                color: Vx.gray600,
+              )),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Container(
+          padding: EdgeInsets.all(12),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                "Kashmore"
+                    .text
+                    .fontFamily("poppins_bold")
+                    .size(32)
+                    .letterSpacing(3)
+                    .make(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      "assets/weather/01d.png",
+                      height: 80,
+                      width: 80,
+                    ),
+                    RichText(
+                        text: TextSpan(children: [
+                      TextSpan(
+                          text: "37$degree",
+                          style: TextStyle(
+                              color: Vx.gray900,
+                              fontSize: 64,
+                              fontFamily: "poppins")),
+                      TextSpan(
+                          text: "sunny",
+                          style: TextStyle(
+                              color: Vx.gray700,
+                              letterSpacing: 3,
+                              fontSize: 14,
+                              fontFamily: "poppins_light")),
+                    ]))
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton.icon(
+                        onPressed: null,
+                        icon: Icon(
+                          Icons.expand_less_rounded,
+                          color: Vx.gray400,
+                        ),
+                        label: "41$degree".text.make()),
+                    TextButton.icon(
+                        onPressed: null,
+                        icon: Icon(
+                          Icons.expand_more_rounded,
+                          color: Vx.gray400,
+                        ),
+                        label: "26$degree".text.make()),
+                  ],
+                ),
+                10.heightBox,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: List.generate(3, (index) {
+                    var IconsList = [clouds, humidity, windspeed];
+                    var values = ["70%", "40", "3.5 km/h"];
+                    return Column(
+                      children: [
+                        Image.asset(
+                          IconsList[index],
+                          width: 60,
+                          height: 60,
+                        )
+                            .box
+                            .gray200
+                            .roundedSM
+                            .padding(EdgeInsets.all(8))
+                            .make(),
+                        10.heightBox,
+                        "${values[index]}".text.gray400.make(),
+                      ],
+                    );
+                  }),
+                ),
+                10.heightBox,
+                Divider(),
+                10.heightBox,
+                SizedBox(
+                  height: 150,
+                  child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: 6,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        padding: EdgeInsets.all(8),
+                        margin: EdgeInsets.only(right: 4),
+                        decoration: BoxDecoration(
+                            color: cardColor,
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Column(
+                          children: [
+                            "${index + 1} AM".text.gray200.make(),
+                            Image.asset(
+                              "assets/weather/09n.png",
+                              width: 80,
+                            ),
+                            "38$degree".text.white.make(),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                10.heightBox,
+                Divider(),
+                10.heightBox,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    "Next 7 Days".text.semiBold.size(16).make(),
+                    TextButton(
+                        onPressed: () {},
+                        child: "View All".text.color(Colors.blue).make())
+                  ],
+                ),
+                ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 7,
+                    itemBuilder: (BuildContext context, int index) {
+                      var day = DateFormat("EEEE").format(
+                          DateTime.now().add(Duration(days: index + 1)));
+                      return Card(
+                        child: Container(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                             Expanded(child:  day.text.semiBold.make()),
+                              Expanded(
+                                child: TextButton.icon(
+                                    onPressed: null,
+                                    icon: Image.asset(
+                                      "assets/weather/50n.png",
+                                      height: 40,
+                                    ),
+                                    label: "26$degree".text.gray800.make()),
+                              ),
+                              RichText(
+                                  text: const TextSpan(children: [
+                                TextSpan(
+                                    text: "38$degree /",
+                                    style: TextStyle(
+                                        color: Vx.gray800,
+                                        fontFamily: "poppins",
+                                        fontSize: 16)),
+                                TextSpan(
+                                    text: " 24$degree",
+                                    style: TextStyle(
+                                        color: Vx.gray600,
+                                        fontFamily: "poppins",
+                                        fontSize: 16)),
+                              ]))
+                            ],
+                          ),
+                        ),
+                      );
+                    })
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          )),
     );
   }
 }
